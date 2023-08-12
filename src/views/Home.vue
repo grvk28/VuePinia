272d2df11a7fa9b7d2450348830bdb1c81152d78
grvk28 <gaurav91226591@gmail.com>
2023-08-12 16:11:10 +0530

    netlify deploy

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+
+import { useAuthStore } from '@/stores';
+
+const authStore = useAuthStore();
+const { user } = storeToRefs(authStore);
+</script>
+
+<template>
+    <div v-if="user">
+        <h1>Hi {{user.firstName}}!</h1>
+        <label>Your Name</label>
+        <p>{{user.firstName}} {{user.lastName}}</p>
+        <label>Your User Id</label>
+        <p>{{user.username}}</p>
+    </div>
+</template>
